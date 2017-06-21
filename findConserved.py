be5c3f5c6cb0a01eb592be7565642f87b41d34dc
John F Lomibao <bm185sae@ieng6-700.ucsd.edu>
2017-06-20 19:25:44 -0700

    changed findConserved.py to new vers.

--- findConserved.py
+++ findConserved.py
@@ -17,39 +17,49 @@ with open(gene_file, 'r') as file:
 	
 with open(bdbh_file, 'r') as file:
 	bdbh_data = [line.strip() for line in file]
-	
-def hasOrtholog(geneToCheck):
+
+#Check if gene has homolog, return the homolog if it does, and 'none' if not
+def hasHomolog(geneToCheck):
 	for i in range(len(bdbh_data)):
-		orth_col = bdbh_data[i].split('\t')
-		if geneToCheck == orth_col[0]:
-			return orth_col[0]
-		if geneToCheck == orth_col[1]:
-			return orth_col[1]
+		hom_col = bdbh_data[i].split('\t')
+		if geneToCheck == hom_col[0]:
+			return hom_col[1]
+		if geneToCheck == hom_col[1]:
+			return hom_col[0]
 	return 'none'
 
-def checkRev(ortholog, gene):
+#Check if last previous 5 genes from homolog is a homolog of a given gene
+def checkRev(homolog, gene):
+	gene_hom = hasHomolog(gene)
 	for i in range(len(gene_data)):
 		gene_col = gene_data[i].split('\t')
-		if ortholog == gene_col[4]:
+		if homolog in gene_col[4]:
 			for j in range(1,6):
 				rev_col = gene_data[i-j].split('\t')
-				if hasOrtholog(rev_col[4]) == gene:
+				if rev_col[4] == gene_hom:
 					return True
 	return False
-print hasOrtholog('NP_414544')
-'''
 #iterate through gene file
 for i in range(len(gene_data)):
 	col = gene_data[i].split('\t')
 	curr_gene = col[4]
-	if hasOrtholog(curr_gene) != 'none':
+	#check if gene has homolog
+	if hasHomolog(curr_gene) != 'none':
+		#if homolog, find next 5 genes
 		for j in range(1, 6):
-			col = gene_data[i+j].split('\t')
-			if hasOrtholog(col[4]) != 'none':
-				curr_orth = hasOrtholog(col[4])
-				if checkRev(curr_orth, curr_gene) == True:
-					print curr_gene+', '+hasOrtholog(curr_orth)+'\t'+hasOrtholog(curr_gene)+', '+curr_orth
-'''
+			next_col = gene_data[i+j].split('\t')
+			#check if those genes have homologs
+			if hasHomolog(next_col[4]) != 'none':
+				curr_hom = hasHomolog(col[4])
+				#check the previous 5 genes of the homologs of the genes found with homologs
+				#if one of those genes is the homolog to curr_gene, then conserved gene has been found
+				'''
+				No pairs of conserved genes were found. May be due to incomplete homolog list as I was not
+				able to finish finding OHTP homologs
+				'''
+				if checkRev(curr_hom, curr_gene):
+					print curr_gene+', '+hasHomolog(curr_orth)+'\t'+hasHomolog(curr_gene)+', '+curr_orth
+
 	
 		
 	
